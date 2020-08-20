edb5bf68feda97cc854a34edbcc7c31b946d8201
Amanda Otero <mandystar018@gmail.com>
2020-08-20 11:59:48 -0700

    Add function in Scripts form and a .maps loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-  $("form#")
+  $("form#converterForm").submit(function(event){
+    event.preventDefault();
+    const romanNum = parseInt($["I", "V", "X", "L", "C", "D", "M"]);
+    // const I = 1;
+    // const V = 5;
+    // const X = 10;
+    // const L = 50;
+    // const C = 100;
+    // const D = 500;
+    // const M = 1,000;
+
+    const newNum = romanNum.map(function(element){
+      return element;
+    });
+
+if (I < 4){
+  $("#result").
+} else if (V < 10 ){
+  
+} else if {
+
+
+}
+
+  });
 });
